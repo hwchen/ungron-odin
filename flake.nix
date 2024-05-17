2ca866979007dc69a02789759e3eb6a9e71a9e2c
Walther Chen <walther.chen@gmail.com>
2024-05-17 16:43:45 -0400

    update odin

--- flake.nix
+++ flake.nix
@@ -8,17 +8,17 @@
     flake-utils.lib.eachDefaultSystem (system: let
       odin-overlay = self: super: {
         odin = super.odin.overrideAttrs (old: rec {
-          version = "nightly-2024-03-31-0d8dadb";
+          version = "nightly-2024-05-17-98f8624";
           src = super.fetchFromGitHub {
             owner = "odin-lang";
             repo = "Odin";
-            rev = "0d8dadb0840ced094383193b7fc22dd86d41e403";
-            sha256 = "sha256-saAUd6gGJWu8rnA0NR4R0UwDvdvjfXlbNfqPhOJpFBM=";
+            rev = "98f8624447a4755f94a0320806a1675f4b47038e";
+            sha256 = "sha256-eRv5kaUAtEJ9Omo97qNEBL1oy5sCErFHfPhaBEyzkDU=";
           };
 
           nativeBuildInputs = with super; [ makeWrapper which ];
 
-          LLVM_CONFIG = "${super.llvmPackages_17.llvm.dev}/bin/llvm-config";
+          LLVM_CONFIG = "${super.llvmPackages_18.llvm.dev}/bin/llvm-config";
           postPatch = ''
             sed -i 's/^GIT_SHA=.*$/GIT_SHA=/' build_odin.sh
             sed -i 's/LLVM-C/LLVM/' build_odin.sh
@@ -31,12 +31,12 @@
 
       ols-overlay = self: super: {
         ols = super.ols.overrideAttrs (old: rec {
-          version = "nightly-2024-03-31-b398c8c";
+          version = "nightly-2024-05-16-e99d7b3";
           src = super.fetchFromGitHub {
             owner = "DanielGavin";
             repo = "ols";
-            rev = "b398c8c817c2b28888e86ebdae84b8deb00a49e0";
-            sha256 = "sha256-EgtqdqDu46254QMwgayBgHzCORMOc5+Vfl6NoAMN+U0=";
+            rev = "e99d7b36013b4fab4defafaef7c9d4cf73d9b94b";
+            sha256 = "sha256-Pi6PE4fau0pi8XmyNlo2AN8a+WvsJtzz1/wU53ytLd8=";
           };
 
           installPhase = old.installPhase;
@@ -55,7 +55,7 @@
         in {
         devShells.default = pkgs.mkShell {
         nativeBuildInputs = [
-        #pkgs.odin
+        pkgs.odin
         #pkgs.ols
         pkgs.poop
         pkgs.hyperfine
